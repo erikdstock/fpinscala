2279b4edc54640b743a0f5f0bc0f132d75d8c265
Erik Stockmeier <erik.d.stock@gmail.com>
2016-08-18 11:06:09 -0400

    add tests from wiki and complete through 3.10

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -23,12 +23,14 @@ object List { // `List` companion object. Contains functions for creating and wo
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+  // Exercise 3.1 - what will be the return?
+  // A: 3.
   val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
+    case Cons(x, Cons(2, Cons(4, _))) => x // no, 2nd tail should start with 3
+    case Nil => 42 // no, would only match Nil
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y // 3: x + y match the first 2 items
+    case Cons(h, t) => h + sum(t) // would match + return x.sum() if previous statement hadn't matched 
+    case _ => 101 // would have matched anything
   }
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
@@ -49,20 +51,215 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  // Ex 3.2: implement tail()
+  // Choices for implementing on Nil: return Nil, return 0, raise or Option(None)
+  // I'm chosing None because I think returning another Nil could be difficult to work with- an empty List with no end.
+  // From my reading of the docs Option(None) fits the expected output better and is more safe
+  // Actually I'm changing this- I think I was misreading the docs Option(None) led to a type mismatch- I thought it would be acceptable
+  //  Final update- I looked at the solution regarding prefering to raise on the tail of an empty list. I was working under the assumption that I was to avoid raising based on chapter 1.
+  //  However I am keeping my implementation (Nil) for further solutions since it is what I came up with and the answer key allows for it.
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => xs
+  }
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  // Ex 3.3: Implement setHead()
+  // I updated the Nil case below after running the wiki's included tests and being told a Nil list should return itself instead of adding a head to it.
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Nil // Cons(h, Nil)
+    case Cons(x, xs) => Cons(h, xs)
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  // Ex 3.4
+  // Around here I remembered to use _ for unused variables
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n == 0) l else l match {
+      case Nil => Nil
+      case Cons(_, xs) => drop(xs, n - 1)
+    }
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  // Ex 3.5
+  // I'm interpreting this question to mean drop while the head meets f(), then stop dropping, not filter()
+  // Answer gives a final default case but I what I came up with is acceptable
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else l
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  // Ex 3.6
+  // Implement init(), should return a list of all elements except the last
+  // Why can't this be implemented in constant time like tail()?
+  // A: It needs to be called recursively to reach the final tail and drop it- O(n).
+  //    tail() matches against the constructor and can return in constant time- O(1).
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  // Ex 3.7
+  // Can product, implemented using foldRight, immediately halt the recursion and return 0.0 if it encounters a 0.0? Why or why not? Consider how any short-circuiting might work if you call foldRight with a large list.
+  // def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _) // original product w/ foldRight
+  // def product3(ns: List[Double]) = {
+  //   def multiply(h: Double, t: List): Double = h match {
+  //     case 0 => {...}
+  //     case _ => h * t
+  //   }
+  //   foldRight(ns, 1.0)(multiply)
+  // }
+  // def product4(ns: List[Double]) = {
+  //   if (list contains 0) 0
+  //   else foldRight(ns, 1.0)(_ * _)
+  // }
+  // I don't see a way I could stop the stack from within the product3 definition since every foldRight has to call itself. I would probably have to scan the list for the value ahead of time (in product3) which would mean an increase from O(n) to O(2n).
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  // 3.8
+  //See what happens when you pass Nil and Cons themselves to foldRight, like this: foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_)).[10] What do you think this says about the relationship between foldRight and the data constructors of List?
+  def exThreeEight[A](l: List[A]) = foldRight(l, Nil:List[A])(Cons(_,_))
+  // When passed a constructor for lists with items and Nil for empty lists, this becomes an identity function. this may be composition?
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  // note- review 3.7-3.8
+
+
+  // Ex 3.9
+  // length using foldRight()
+  // signature: foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+  def length[A](l: List[A]): Int = {
+    foldRight(l, 0)((_, b) => 1 + b)
+  }
+  /* trace for List(1, 2, 3)
+    1 + foldRight(List(2, 3))((a, b) => 1 + b)
+    1 + 1 + foldRight(List(3)) ...
+    1 + 1 + 1 + foldRight(List()) ...
+    1 + 1 + 1 + 0
+  */
+
+  // ex 3.10 foldLeft with tailrec
+  // my solution is identical to the answer minus the use of the extra val- answer key says
+  // foldLeft(t, f(z,h))(f) - I think this is just a mindset I need to get into.
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) => {
+      val next = f(z, h)
+      foldLeft(t, next)(f)
+    }
+  }
 
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def test_sum(sum: List[Int] => Int): Unit =
+  {
+    assert( sum(           Nil ) ==  0, "sum of empty list should be 0")
+    assert( sum(       List(5) ) ==  5, "sum of single-element list should be the element" )
+    assert( sum( List(1,2,3,4) ) == 10, "sum of list should be sum of its elements" )
+  }
+
+  def test_sum(): Unit = test_sum(sum)
+  def test_sum2(): Unit = test_sum(sum2)
+
+  def test_product(product: List[Double] => Double): Unit =
+  {
+    assert( product( Nil)                       ==  1.0,  "product of empty list should be 1.0" )
+    assert( product( List(7.0))                 ==  7.0,  "product of single-element list should be the element" )
+    assert( product( List(1.0, 2.0, 3.0, 4.0) ) == 24.0,  "product of list should be product of its elements" )
+    assert( product( List(1.0, 2.0, 0.0, 4.0) ) ==  0.0,  "product of list containing zero should be zero" )
+  }
+
+  def test_product(): Unit = test_product(product)
+  def test_product2(): Unit = test_product(product2)
+
+  def test_append(): Unit =
+  {
+    assert( append( Nil,             Nil ) ==           Nil, "append of two empty lists should be empty list")
+    assert( append( Nil,         List(3) ) ==       List(3), "append of empty list to a list should be list")
+    assert( append( List(3),         Nil ) ==       List(3), "append of list to empty list should be list")
+    assert( append( List(1,2),   List(3) ) ==   List(1,2,3), "append of list to one-element list should be concatenation of lists")
+    assert( append( List(1),   List(2,3) ) ==   List(1,2,3), "append of one-element list to list should be concatenation of lists")
+    assert( append( List(1,2), List(3,4) ) == List(1,2,3,4), "append of two lists should be concatenation of lists")
+  }
+
+  def test_tail(): Unit =
+  {
+    assert( tail(         Nil ) ==       Nil, "tail of Nil should be Nil")
+    assert( tail(     List(3) ) ==       Nil, "tail of single-element list should be Nil")
+    assert( tail( List(1,2,3) ) == List(2,3), "tail of list should be rest")
+  }
+
+  def test_setHead(): Unit =
+  {
+    assert( setHead(       Nil, 1 ) ==       Nil, "setHead of empty list should be empty list")
+    assert( setHead(   List(2), 1 ) ==   List(1), "setHead of single-element list should be two-element list")
+    assert( setHead( List(3,2), 1 ) == List(1,2), "setHead of two-element list should be three-element list")
+  }
+
+  def test_drop(): Unit =
+  {
+    assert( drop( Nil,          0) ==         Nil, "drop of zero elements from empty list is empty list")
+    assert( drop( Nil,          1) ==         Nil, "drop of one element from empty list is empty list")
+    assert( drop( Nil,         10) ==         Nil, "drop of many elements from empty list is empty list")
+    assert( drop( List(3),      0) ==     List(3), "drop of zero elements from single-element list is the list")
+    assert( drop( List(3),      1) ==         Nil, "drop of one element from single-element list is empty list")
+    assert( drop( List(3),     10) ==         Nil, "drop of many elements from single-element list is empty list")
+    assert( drop( List(1,2,3),  0) == List(1,2,3), "drop of zero elements from list is list")
+    assert( drop( List(1,2,3),  1) ==   List(2,3), "drop of one elements from list is list without 1st element")
+    assert( drop( List(1,2,3),  2) ==     List(3), "drop of n elements from list is list without 1st n elements")
+    assert( drop( List(1,2,3), 10) ==         Nil, "drop of too many elements from list is empty list")
+  }
+
+  def test_dropWhile(): Unit =
+  {
+    val positive = (x: Int) => x > 0
+    assert( dropWhile(                  Nil, positive ) ==                  Nil, "dropWhile of empty list should be empty list")
+    assert( dropWhile(              List(1), positive ) ==                  Nil, "dropWhile of list with single valid element should be empty list")
+    assert( dropWhile( List( 1,  2,  3,  4), positive ) ==                  Nil, "dropWhile of list with only valid elements should be empty list")
+    assert( dropWhile( List( 1,  2, -3,  4), positive ) ==          List(-3, 4), "dropWhile of list with two leading valid elements should be list without leading elements")
+    assert( dropWhile( List( 1, -2, -3,  4), positive ) ==      List(-2, -3, 4), "dropWhile of list with one leading valid element should be list without leading element")
+    assert( dropWhile( List(-1, -2, -3,  4), positive ) ==  List(-1, -2, -3, 4), "dropWhile of list with no leading valid elements should be same list")
+    assert( dropWhile( List(-1, -2, -3, -4), positive ) == List(-1, -2, -3, -4), "dropWhile of list with no valid elements should be Nil")
+  }
+
+  def test_init(): Unit =
+  {
+    assert( init(         Nil ) ==       Nil, "init of empty list should be empty list")
+    assert( init(     List(3) ) ==       Nil, "init of single-element-list should be empty list")
+    assert( init( List(1,2,3) ) == List(1,2), "init of list should not have last element")
+  }
+
+  def test_length(): Unit =
+  {
+    assert( length(         Nil ) == 0, "length of empty list is zero")
+    assert( length(     List(1) ) == 1, "length of single-element list is one")
+    assert( length( List(1,2,3) ) == 3, "length of n-element list is n")
+  }
+
+  def test_foldLeft(): Unit =
+  {
+    assert( foldLeft(  List(1, 2, 3, 4, 5), 0) (_ + _) ==
+      foldRight( List(1, 2, 3, 4, 5), 0) (_ + _),
+      "foldLeft should compute the same sum value as foldRight")
+
+    assert( foldLeft(  List(1, 2, 3, 4, 5), 1) (_ * _) ==
+      foldRight( List(1, 2, 3, 4, 5), 1) (_ * _),
+      "foldLeft should compute the same product value as foldRight")
+
+    assert( foldLeft(  List("a", "b", "c"), "") (_ + _) ==
+      foldRight( List("a", "b", "c"), "") (_ + _),
+      "foldLeft should compute the same concatenation value as foldRight")
+  }
+
+  def test(): Unit = {
+    test_sum
+    test_sum2
+    test_product
+    test_product2
+    test_append
+    test_tail
+    test_setHead
+    test_drop
+    test_dropWhile
+    test_init
+    test_length
+    test_foldLeft
+  }
 }
