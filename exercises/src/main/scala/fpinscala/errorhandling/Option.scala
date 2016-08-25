4e5be50f53bc68d8e3f59bfd23d735ad49dd64f4
Erik Stockmeier <erik.d.stock@gmail.com>
2016-08-25 19:19:36 -0400

    first ex from ch 4

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,13 +4,33 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  // got this one wrong. correct body is :
+  // def flatMap[B](f: A => Option[B]): Option[B] = 
+  //   map(f) getOrElse None
+  // so basically this is returning the value without the option container.
+  // I think? This is weird. signature is different from map. maybe revisit TODO
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    // map(f) getOrElse None // correct answer
+    case None => None
+    case Some(a) => f(a)
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  // LOVE the 0 explanation for whatever these functions might do.
+  // actually it is in the book.
+  def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case Some(a) => this
+  }
 
   def filter(f: A => Boolean): Option[A] = sys.error("todo")
 }
